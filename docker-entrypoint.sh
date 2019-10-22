b11e60ad28d466a9cd19cd98e84148d22637312b
Almino <almino.melo@gmail.com>
2019-10-22 15:41:00 -0300

    Decide entre `install` e `ci`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 set -ex
-# Checks if node_modules exists and run [npm ci](https://docs.npmjs.com/cli/ci.html)
-[ ! -d "node_modules" ] && npm ci
+# Checks if node_modules exists and runs [npm ci](https://docs.npmjs.com/cli/ci.html)
+if [ ! -f "package-lock.json" ];
+then
+    npm install
+else
+    [ ! -d "node_modules" ] && npm ci
+fi
 $(npm bin)/nuxt
 
 exec "$@"
